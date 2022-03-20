e2257f98dee374c3c80a136a5aa954d81c3cec48
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 21:36:30 +0700

    Post Processor

---
+++ Spring-Basic-Code/spring-basic/src/main/java/com/gusrylmubarok/springbasic/processor/FooBeanFactoryPostProcessor.java
@@ -0,0 +1,32 @@
+package com.gusrylmubarok.springbasic.processor;
+
+import com.gusrylmubarok.springbasic.data.Foo;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+@Component
+public class FooBeanFactoryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        GenericBeanDefinition definition = new GenericBeanDefinition();
+        definition.setScope("singleton");
+        definition.setBeanClass(Foo.class);
+
+        registry.registerBeanDefinition("foo", definition);
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        // nothing
+    }
+}
