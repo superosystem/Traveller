2c42a5761ba5ef560731c23d1c6d9e9c9b98c822
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 21:32:43 +0700

    Bean Factory

---
+++ Spring-Basic-Code/spring-basic/src/test/java/com/gusrylmubarok/springbasic/BeanFactoryTest.java
@@ -0,0 +1,41 @@
+package com.gusrylmubarok.springbasic;
+
+import com.gusrylmubarok.springbasic.data.Foo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+public class BeanFactoryTest {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(ScanConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testBeanFactory() {
+
+        ObjectProvider<Foo> fooObjectProvider = applicationContext.getBeanProvider(Foo.class);
+        List<Foo> fooList = fooObjectProvider.stream().collect(Collectors.toList());
+
+        System.out.println(fooList);
+
+        Map<String, Foo> beans = applicationContext.getBeansOfType(Foo.class);
+        System.out.println(beans);
+
+    }
+}
