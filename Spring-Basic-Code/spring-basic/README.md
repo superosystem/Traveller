a09ec7b6c6ca5894437f1d86e3badca7e2101640
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 13:22:37 +0700

    Application Context

---
+++ Spring-Basic-Code/spring-basic/README.md
@@ -0,0 +1,5 @@
+## Spring Basic Code
+This is example code for Spring Basic
+## What I have learned
+1. Inversion of Control
+2. 
---
+++ Spring-Basic-Code/spring-basic/src/main/java/com/gusrylmubarok/springbasic/HelloWorldConfiguration.java
@@ -0,0 +1,12 @@
+package com.gusrylmubarok.springbasic;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+@Configuration
+public class HelloWorldConfiguration {
+}
