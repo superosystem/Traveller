7f83abc1b9958003ee90c6d1eeb9fdf271484b00
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 18:13:48 +0700

    Dependency Injection

---
+++ Spring-Basic-Code/spring-basic/src/main/java/com/gusrylmubarok/springbasic/di/DependencyInjectionConfiguration.java
@@ -0,0 +1,32 @@
+package com.gusrylmubarok.springbasic.di;
+
+import com.gusrylmubarok.springbasic.data.Bar;
+import com.gusrylmubarok.springbasic.data.Foo;
+import com.gusrylmubarok.springbasic.data.FooBar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+@Configuration
+public class DependencyInjectionConfiguration {
+
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        return new Bar();
+    }
+
+    @Bean
+    public FooBar fooBar(Foo foo, Bar bar) {
+        return new FooBar(foo, bar);
+    }
+
+}
