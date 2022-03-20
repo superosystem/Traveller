b5d276bb436ecdfa466ac6cc92531aa0b362d218
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 20:30:26 +0700

    Doubleton Scopes

--- Spring-Basic-Code/spring-basic/src/test/java/com/gusrylmubarok/springbasic/ScopeTest.java
+++ Spring-Basic-Code/spring-basic/src/test/java/com/gusrylmubarok/springbasic/ScopeTest.java
@@ -1,6 +1,7 @@
 package com.gusrylmubarok.springbasic;
 
 import com.gusrylmubarok.springbasic.bean.scopes.ScopeConfiguration;
+import com.gusrylmubarok.springbasic.data.Bar;
 import com.gusrylmubarok.springbasic.data.Foo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,6 +25,7 @@ public class ScopeTest {
 
     @Test
     void testPrototypeScope() {
+
         Foo foo1 = applicationContext.getBean(Foo.class);
         Foo foo2 = applicationContext.getBean(Foo.class);
         Foo foo3 = applicationContext.getBean(Foo.class);
@@ -31,6 +33,22 @@ public class ScopeTest {
         Assertions.assertNotSame(foo1, foo2);
         Assertions.assertNotSame(foo1, foo3);
         Assertions.assertNotSame(foo2, foo3);
+
+    }
+
+    @Test
+    void testDoubletonScope() {
+
+        Bar bar1 = applicationContext.getBean(Bar.class);
+        Bar bar2 = applicationContext.getBean(Bar.class);
+        Bar bar3 = applicationContext.getBean(Bar.class);
+        Bar bar4 = applicationContext.getBean(Bar.class);
+
+        Assertions.assertSame(bar1, bar3);
+        Assertions.assertSame(bar2, bar4);
+
+        Assertions.assertNotSame(bar1, bar2);
+        Assertions.assertNotSame(bar3, bar4);
 
     }
 
