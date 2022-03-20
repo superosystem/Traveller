0eaf5b3bc6bf631d3d6bb4a9485f28fcc45cd0ba
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 20:14:14 +0700

    DependsOn Dependency

---
+++ Spring-Basic-Code/spring-basic/src/main/java/com/gusrylmubarok/springbasic/di/DependsOnConfiguration.java
@@ -0,0 +1,34 @@
+package com.gusrylmubarok.springbasic.di;
+
+import com.gusrylmubarok.springbasic.data.Bar;
+import com.gusrylmubarok.springbasic.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn({
+            "bar"
+    })
+    public Foo foo() {
+        log.info("Create new foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        log.info("Create new foo");
+        return new Bar();
+    }
+
+}
