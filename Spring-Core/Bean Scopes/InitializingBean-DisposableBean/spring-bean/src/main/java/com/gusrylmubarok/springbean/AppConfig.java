1a1b0ef1a3bc47e6a6518ab4c3aa2430fa986cac
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-20 15:29:28 +0700

    Spring Bean InitializingBean and DisposableBean

---
+++ Spring-Core/Bean Scopes/InitializingBean-DisposableBean/spring-bean/src/main/java/com/gusrylmubarok/springbean/AppConfig.java
@@ -0,0 +1,15 @@
+package com.gusrylmubarok.springbean;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+@Configuration
+@ComponentScan(basePackages = "com.gusrylmubarok.springbean")
+public class AppConfig {
+
+}
---
+++ Spring-Core/Bean Scopes/InitializingBean-DisposableBean/spring-bean/src/test/java/com/gusrylmubarok/springbean/SpringBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusrylmubarok.springbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
