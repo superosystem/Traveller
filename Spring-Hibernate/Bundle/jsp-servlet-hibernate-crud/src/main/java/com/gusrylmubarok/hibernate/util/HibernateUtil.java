269a2b550fc5751e69d701843c7530dc9f0409ed
ophthalmologist <gusrylmubarok@gmail.com>
2022-03-23 00:42:31 +0700

    Java JSP Hibernate Mysql

---
+++ Spring-Hibernate/Bundle/jsp-servlet-hibernate-crud/src/main/java/com/gusrylmubarok/hibernate/util/HibernateUtil.java
@@ -0,0 +1,54 @@
+package com.gusrylmubarok.hibernate.util;
+
+import com.gusrylmubarok.hibernate.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+/**
+ * @author Gusryl Mubarok
+ * @email gusrylmubarok@gmail.com
+ */
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db?useSSL=false&serverTimezone=Asia/Jakarta");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                System.out.println("Hibernate Java Config serviceRegistry created");
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                return sessionFactory;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
