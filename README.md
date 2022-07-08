83d60340194903a0b6672a8b29f567f7713a7b0c
gusrylmubarok <gusrylmubarok@gmail.com>
2022-07-08 16:51:10 +0700

    Rewrite

---
+++ README.md
@@ -0,0 +1,3 @@
+# Health Application - Backend
+
+...
--- src/main/java/com/gusrylmubarok/sms/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gusrylmubarok.sms.repository;
-
-import com.gusrylmubarok.sms.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
