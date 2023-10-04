296b1973cdadb8632af27b671bcfaf6895831e51
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-10-04 21:54:31 +0700

    feat(ml): update model training

---
+++ machine_learning/app/flask/model.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from modules.feature_extraction import ResNet_FeatureExtractor, VGG_FeatureExtractor
+
+
+class Model(keras.models.Model):
+    def __init__(self, opt, **kwargs):
+        super(Model, self).__init__(**kwargs)
+        self.opt = opt
+
+        print("No Transformation Module")
+
+        if opt.FeatureExtraction == "VGG":
+            if opt.pretrained:
+                self.FeatureExtraction = keras.applications.vgg16.VGG16(
+                    include_top=False, weights="imagenet", input_shape=(opt.imgH, opt.imgW, 3)
+                )
+                # for layer in self.FeatureExtraction.layers:
+                #     layer.trainable = False
+            else:
+                self.FeatureExtraction = VGG_FeatureExtractor(opt.output_channel, input_shape=(opt.imgH, opt.imgW, 1))
+        elif opt.FeatureExtraction == "ResNet":
+            self.FeatureExtraction = ResNet_FeatureExtractor(opt.output_channel)
+        self.FeatureExtraction_output = opt.output_channel
+        # untuk sekarang
+        if opt.FeatureExtraction == "VGG":
+            if opt.pretrained:
+                self.AdaptiveAvgPool = tfa.layers.AdaptiveAveragePooling2D(
+                    output_size=(3, 1)
+                )
+            else:
+                self.AdaptiveAvgPool = tfa.layers.AdaptiveAveragePooling2D(
+                    output_size=(49, 1)
+                )
+        elif opt.FeatureExtraction == "ResNet":
+            self.AdaptiveAvgPool = tfa.layers.AdaptiveAveragePooling2D(
+                output_size=(23, 1)
+            )
+        print("No sequence modelling module specified")
+        self.SequenModelling_output = self.FeatureExtraction_output
+        self.flatten = layers.Flatten()
+        self.Prediction = layers.Dense(opt.num_class)
+
+    def call(self, X, text, training=None):
+
+        """Feature Extraction Stage"""
+        visual_feature = self.FeatureExtraction(X)
+        visual_feature = self.AdaptiveAvgPool(
+            tf.transpose(visual_feature, perm=[0, 2, 1, 3])
+        )  # [b, w, h, c] -> [b, h, w, c]
+        visual_feature = tf.squeeze(visual_feature, axis=2)
+
+        # if self.opt.pretrained:
+        #     contextual_feature = self.flatten(visual_feature)
+        # else:
+        contextual_feature = visual_feature
+        prediction = self.Prediction(contextual_feature)
+        return prediction
---
+++ machine_learning/app/flask/modules/prediction.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class AttentionCell(keras.models.Model):
+    def __init__(self, hidden_size, num_embeddings):
+        super().__init__()
+        self.i2h = layers.Dense(hidden_size, use_bias=False)
+        self.h2h = layers.Dense(hidden_size)
+        self.score = layers.Dense(1, use_bias=False)
+        self.rnn = layers.LSTMCell()
+        self.hidden_size = hidden_size
+
+    def call(self, prev_hidden, batch_H, char_onehots):
+        # [batch_size x num_encoder_step x num_channel] -> [batch_size x num_encoder_step x hidden_size]
+        batch_H_proj = self.i2h(batch_H)
+        prev_hidden_proj = tf.expand_dims(self.h2h(prev_hidden[0]), axis=1)
+        e = self.score(
+            tf.math.tanh(tf.add(batch_H_proj + prev_hidden_proj))
+        )  # rawan error
+
+        alpha = tf.math.softmax(e, axis=1)
+        context = tf.squeeze(tf.matmul(tf.transpose(alpha, [0, 2, 1]), batch_H), axis=1)
+        concat_context = tf.concat([context, char_onehots], axis=1)
+        cur_hidden = self.rnn(concat_context, prev_hidden)  # rawan error
+        return cur_hidden, alpha
+
+
+class Attention(keras.models.Model):
+    def __init__(self, hidden_size, num_classes):
+        super().__init__()
+        self.attention_cell = AttentionCell()
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.generator = layers.Dense(num_classes)
+
+    @staticmethod
+    def _char_to_onehot(input_char, onehot_dim=38):
+        input_char = tf.expand_dims(input_char, axis=1)
+        one_hot = tf.one_hot(input_char, onehot_dim)
+        return one_hot  # rawan error
+
+    def call(self, batch_H, text, training=True, batch_max_length=25):
+        """
+        input:
+            batch_H : contextual_feature H = hidden state of encoder. [batch_size x num_steps x contextual_feature_channels]
+            text : the text-index of each image. [batch_size x (max_length+1)]. +1 for [GO] token. text[:, 0] = [GO].
+        output: probability distribution at each step [batch_size x num_steps x num_classes]
+        """
+        batch_size = batch_H.shape[0]
+        num_steps = batch_max_length + 1
+
+        output_hiddens = tf.zeros(shape=[batch_size, num_steps, self.hidden_size])
+        hidden = (
+            tf.zeros(shapes=[batch_size, self.hidden_size]),
+            tf.zeros(shapes=[batch_size, self.hidden_size]),
+        )
+
+        if training:
+            for i in range(num_steps):
+                # one-hot vectors for a i-th char. in a batch
+                char_onehots = self._char_to_onehot(
+                    text[:, i], onehot_dim=self.num_classes
+                )
+                # hidden : decoder's hidden s_{t-1}, batch_H : encoder's hidden H, char_onehots : one-hot(y_{t-1})
+                hidden, alpha = self.attention_cell(hidden, batch_H, char_onehots)
+                output_hiddens[:, i, :] = hidden[0]
+
+            probs = self.generator(output_hiddens)
+
+        else:
+            targets = tf.zeros(shape=[batch_size], dtypes=tf.float64)
+            probs = tf.zeros(shape=[batch_size, num_steps, self.num_classes])
+
+            for i in range(num_steps):
+                char_onehots = self._char_to_onehot(
+                    targets, onehot_dim=self.num_classes
+                )
+                hidden, alpha = self.attention_cell(hidden, batch_H, char_onehots)
+                probs_step = self.generator(hidden[0])
+                probs[:, i, :] = probs_step
+                _, next_input = tf.reduce_max(probs_step, axis=1)  # rawan error
+                targets = next_input
+
+        return probs
